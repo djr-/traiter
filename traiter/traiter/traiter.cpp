202844ced993de6db879d9a30ac5cc4336583ac8
Drew Ryder <drewjryder@gmail.com>
2015-03-21 09:56:38 -0500

    Initial creation of traiter solution

---
+++ traiter/traiter/traiter.cpp
@@ -0,0 +1,18 @@
+//#include <opencv2/core/core.hpp>
+//#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+
+//using namespace cv;
+//
+int main()
+{
+	//	Mat image = imread("roots.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	//
+	//	//threshold(image, image, 120, 255, 3);
+	//	adaptiveThreshold(image, image, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 5, 0);
+	//	imshow("roots", image);
+	//	waitKey();
+	//
+	return 0;
+}
