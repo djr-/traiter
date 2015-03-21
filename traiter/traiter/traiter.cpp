5450a3d0466b056a1fa4916597ae4731a63dfd75
Drew Ryder <drewjryder@gmail.com>
2015-03-21 10:07:50 -0500

    Added OpenCV dependencies and simple example of threshold and adaptive threshold

--- traiter/traiter/traiter.cpp
+++ traiter/traiter/traiter.cpp
@@ -1,18 +1,22 @@
-//#include <opencv2/core/core.hpp>
-//#include <opencv2/highgui/highgui.hpp>
-//#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 
-//using namespace cv;
-//
+using namespace cv;
+
 int main()
 {
-	//	Mat image = imread("roots.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-	//
-	//	//threshold(image, image, 120, 255, 3);
-	//	adaptiveThreshold(image, image, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 5, 0);
-	//	imshow("roots", image);
-	//	waitKey();
-	//
+	Mat image = imread("roots.jpg", CV_LOAD_IMAGE_GRAYSCALE);	//TODO_DR: Replace with a try_read
+	Mat thresholdImage = image.clone();
+	Mat adaptiveThresholdImage = image.clone();
+	
+	threshold(image, thresholdImage, 120, 255, 3);
+	adaptiveThreshold(image, adaptiveThresholdImage, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 5, 0);
+	imshow("original", image);
+	imshow("threshold", thresholdImage);
+	imshow("adaptive threshold", adaptiveThresholdImage);
+	waitKey();
+	
 	return 0;
 }
